7422b68e83cebb90e945f672228ef358e6287024
Felipe Suero <suerofrancisco@yahoo.com>
2023-04-20 02:23:10 -0400

    good

--- app/models/concerns/imgur_scan_existing_threads.rb
+++ app/models/concerns/imgur_scan_existing_threads.rb
@@ -6,7 +6,8 @@ class ImgurScanExistingThreads
         logger = Logger.new('log/logfile.log')
         imgur_helper = ImgurHelper.new
         Sathread.all.each do |thread|
-            logger.debug('scan') { "Scanning thread #{thread.id}" }
+            # logger.debug('scan') { "Scanning thread #{thread.id}" }
+            p "Scanning thread #{thread.thread_id} title: #{thread.title}"
             scan_specific_thread(thread.thread_id)
         end
     end
